dcc7814354f11e07957b096407819df9406552d2
Sharonz! <81288171+MrNobody503@users.noreply.github.com>
2021-05-26 16:32:00 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -1,4 +1,4 @@
-# (c) @EverythingSuckz | @AbirHasan2005
+# (c) @MrNobody503
 
 from WebStreamer.bot import StreamBot
 from WebStreamer.vars import Var
@@ -26,7 +26,7 @@ async def start(b, m):
                 if user.status == "kicked":
                     await b.send_message(
                         chat_id=m.chat.id,
-                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                        text="Sorry Sir, You are Banned to use me. Contact my [Developer](https://t.me/MrNobody_Here).",
                         parse_mode="markdown",
                         disable_web_page_preview=True
                     )
@@ -48,7 +48,7 @@ async def start(b, m):
             except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+                    text="Something went Wrong. Contact my [Developer](https://t.me/MrNobody_Here).",
                     parse_mode="markdown",
                     disable_web_page_preview=True)
                 return
@@ -56,8 +56,7 @@ async def start(b, m):
             text='🙋 Hey Bruh!!\nI am Instant Telegram File to Link Generator Bot.\n\nSend me any file & see the magic!',
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton('Bots Channel', url='https://t.me/Discovery_Updates'), InlineKeyboardButton('Support Group', url='https://t.me/linux_repo')],
-                    [InlineKeyboardButton('Developer', url='https://t.me/AbirHasan2005')]
+                    [InlineKeyboardButton('Bots Channel', url='https://t.me/Nobodys_Bots'), InlineKeyboardButton('Developer', url='https://t.me/MrNobody_Here')]
                 ]
             ),
             disable_web_page_preview=True
@@ -85,7 +84,7 @@ async def start(b, m):
                             ],
                             [
                                 InlineKeyboardButton("🔄 Refresh / Try Again",
-                                                     url=f"https://t.me/AH_File2Link_Bot?start=AbirHasan2005_{usr_cmd}")
+                                                     url=f"https://t.me/File2LinkPro_Bot?start=MrNobody_Here_{usr_cmd}")
                             ]
                         ]
                     ),
@@ -95,7 +94,7 @@ async def start(b, m):
             except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+                    text="Something went Wrong. Contact my [Developer](https://t.me/MrNobody_Here).",
                     parse_mode="markdown",
                     disable_web_page_preview=True)
                 return
@@ -145,7 +144,7 @@ async def help_handler(bot, message):
             if user.status == "kicked":
                 await bot.send_message(
                     chat_id=message.chat.id,
-                    text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                    text="Sorry Sir, You are Banned to use me. Contact my [Developer](https://t.me/MrNobody_Here).",
                     parse_mode="markdown",
                     disable_web_page_preview=True
                 )
@@ -167,7 +166,7 @@ async def help_handler(bot, message):
         except Exception:
             await bot.send_message(
                 chat_id=message.chat.id,
-                text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+                text="Something went Wrong. Contact my [Developer](https://t.me/MrNobody_Here).",
                 parse_mode="markdown",
                 disable_web_page_preview=True)
             return
@@ -177,8 +176,8 @@ async def help_handler(bot, message):
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo"), InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")],
-                [InlineKeyboardButton("Developer", url="https://t.me/AbirHasan2005")]
+                [InlineKeyboardButton("Channel", url="https://t.me/Nobodys_Bots"), 
+                [InlineKeyboardButton("Developer", url="https://t.me/MrNobody_Here")]
             ]
         )
     )
